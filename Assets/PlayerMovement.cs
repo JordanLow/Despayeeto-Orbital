7f59ace1406c738975149f7ab5d4f71298777a8a
Jordan Low <jordanlow.jinyi@gmail.com>
2025-05-06 22:26:48 +0800

    Free Camera & Relative Movement added. TODO: Setup the player state machine

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,35 +6,42 @@ public class PlayerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     
 	[SerializeField] float jumpSpeed = 4f;
-	[SerializeField] float moveSpeed = 10f;
+	[SerializeField] float moveSpeed = 8f;
+	[SerializeField] float turnSpeed = 10f;
 	
 	private CharacterController controller;
-	private Vector3 curVel;
+	private Vector3 relativeDir;
 	
 	void Start()
     {
         controller = GetComponent<CharacterController>();
-		curVel = Vector3.zero;
+		relativeDir = Vector3.zero;
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (curVel.y > Physics.gravity.y) curVel.y += Physics.gravity.y * Time.deltaTime;
-        controller.Move(curVel * Time.deltaTime);
+		Vector3 cameraForward = Camera.main.transform.forward; 
+		cameraForward.y = 0;
+        Vector3 cameraRight = Camera.main.transform.right; 
+		cameraRight.y = 0;
+		if (relativeDir.y > Physics.gravity.y) relativeDir.y += Physics.gravity.y * Time.deltaTime;
+		Vector3 curDir = relativeDir.z * cameraForward + relativeDir.x * cameraRight;
+		curDir.y = relativeDir.y;
+		controller.Move(curDir * Time.deltaTime);
+		Vector3 curFaceDirection = curDir;
+		curFaceDirection.y = 0;
+		if (curFaceDirection == Vector3.zero) return;
+		transform.rotation = Quaternion.LookRotation(curFaceDirection);
     }
 	
 	public void OnMove(InputValue val) {
 		Vector2 direction = val.Get<Vector2>() * moveSpeed;
-		curVel.x = direction.x;
-		curVel.z = direction.y;
+		relativeDir.x = direction.x;
+		relativeDir.z = direction.y;
 	}
 	
 	public void OnJump() {
-		curVel.y = jumpSpeed;
-	}
-	
-	public void OnPan(InputValue val) {
-		Vector2 angle = val.Get<Vector2>();
+		relativeDir.y = jumpSpeed;
 	}
 }
